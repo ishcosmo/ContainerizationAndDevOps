f502513a9e445bad340039da7f688fcdfa33fac4
ishcosmo <ishitasharma6119@gmail.com>
2026-05-02 09:51:35 +0000

    practical

--- LAB/Experiment7/my-app/app.py
+++ LAB/Experiment7/my-app/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     #return "Hello from CI/CD Pipeline!"
-    return "Hello from CI/CD Pipeline!, my sapid is 500119435"
+    return "Hello from CI/CD Pipeline!, my sapid is 500119435 its ishita"
 
 app.run(host="0.0.0.0", port=80)
